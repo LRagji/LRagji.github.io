aed33699e3c667b65fdec226fbd3b18706e1c055
LRagji <Laukik.Ragji@live.com>
2018-09-28 21:45:28 +0530

    Added lite-elements xperiment

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("JS Works");
+let express = require('express')
+let app = express()
+let port = 5000;
+
+app.use(express.static('./xperimental/lite-elements'))
+
+app.listen(port, () => console.log(`Dev server listening on port ${port}!`))
