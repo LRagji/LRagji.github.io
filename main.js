8c5087d6f7d42dee61d36d6ab26f4e529a56042b
LRagji <Laukik.Ragji@live.com>
2018-09-29 14:00:24 +0530

    Changed the dev server to point to root index

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ let express = require('express')
 let app = express()
 let port = 5000;
 
-app.use(express.static('./xperimental/lit-elements'))
+app.use(express.static('./'))
 
 app.listen(port, () => console.log(`Dev server listening on port ${port}!`))
