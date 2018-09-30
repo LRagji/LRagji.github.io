f0b99e76242da48bfb88dfa9c1e93f1e41c31bde
LRagji <Laukik.Ragji@live.com>
2018-09-30 15:48:59 +0530

    Rectified property values, added a dispose method.

--- xperimental/lit-elements/gyro-element.js
+++ xperimental/lit-elements/gyro-element.js
@@ -7,73 +7,84 @@ class gyroElement extends LitElement {
   static get properties() {
     return {
       alpha: {
-        type: Number
+        type: Number,
+        value: 0
       },
       beta: {
-        type: Number
+        type: Number,
+        value: 0
       },
       gamma: {
-        type: Number
+        type: Number,
+        value: 0
+      },
+      alphaSensitivity: {
+        type: Number,
+        value: 1
+      },
+      betaSensitivity: {
+        type: Number,
+        value: 1
+      },
+      gammaSensitivity: {
+        type: Number,
+        value: 1
       },
       isSupported: {
-        type: Boolean
+        type: Boolean,
+        value: false
       }
     };
   }
 
   constructor() {
     super();
-    //Setting Default values of all properties
-    this.isSupported = false;
-    this.alpha = 0;
-    this.beta = 0;
-    this.gamma = 0;
+
+    this.setupDefaultValuesForProperties();
 
     //Setting context for all methods
     this.handleMotion = this.handleMotion.bind(this);
-    this.log = this.log.bind(this);
+    this.dispose = this.dispose.bind(this);
 
     if (window.DeviceMotionEvent) {
-      window.addEventListener('devicemotion', this.handleMotion);
-      // setInterval(() => {
-      //   this.handleMotion({
-      //     rotationRate: {
-      //       alpha: (Math.random() * 100),
-      //       beta: (Math.random() * 100),
-      //       gamma: (Math.random() * 100)
-      //     }
-      //   })
-      // }, 1000);
+      //window.addEventListener('devicemotion', this.handleMotion);
+      setInterval(() => {
+        this.handleMotion({
+          rotationRate: {
+            alpha: (Math.random() * 100),
+            beta: (Math.random() * 100),
+            gamma: (Math.random() * 100)
+          }
+        })
+      }, 1000);
     }
   }
 
-  log(message) {
-    this.shadowRoot.getElementById('log').innerHTML += message + "<br/>";
+  setupDefaultValuesForProperties() {
+    this.gamma = this.beta = this.alpha = 0;
+    this.gammaSensitivity = this.betaSensitivity = this.alphaSensitivity = 1;
+  }
+
+  dispose() {
+    window.removeEventListener('devicemotion', this.handleMotion);
   }
 
   handleMotion(e) {
-    // if (e.rotationRate.alpha & e.rotationRate.beta & e.rotationRate.gamma) {
-    //   this.isSupported = false;
-    // } else {
-    // alert("Event:" + e.rotationRate.alpha);
-    let logMessage = "";
-    if (Math.abs(this.alpha - e.rotationRate.alpha) > 1) {
-      logMessage = `alpha:${e.rotationRate.alpha} `;
+
+    if (Math.abs(this.alpha - e.rotationRate.alpha) > this.alphaSensitivity) {
       this.alpha = Math.round(e.rotationRate.alpha);
+      if (this.isSupported !== true) this.isSupported = true;
     }
-    if (Math.abs(this.beta - e.rotationRate.beta) > 1) {
-       logMessage += `beta:${e.rotationRate.beta} `;
+
+    if (Math.abs(this.beta - e.rotationRate.beta) > this.betaSensitivity) {
       this.beta = Math.round(e.rotationRate.beta);
+      if (this.isSupported !== true) this.isSupported = true;
     }
-    if (Math.abs(this.gamma - e.rotationRate.gamma) > 1) {
-      logMessage += `gamma:${e.rotationRate.gamma} `;
+
+    if (Math.abs(this.gamma - e.rotationRate.gamma) > this.gammaSensitivity) {
       this.gamma = Math.round(e.rotationRate.gamma);
+      if (this.isSupported !== true) this.isSupported = true;
     }
-    if (this.isSupported != true)
-      this.isSupported = true;
-    //alert(this.alpha);
-    //}
-    if (logMessage !== "") this.log(logMessage);
   }
 
   render() {
@@ -90,8 +101,7 @@ class gyroElement extends LitElement {
   } </style>
       <div class="level" style="transform: rotate(${this.alpha}deg);"><div class="leveltext">${this.alpha}deg</div></div>
       <div class="level" style="transform: rotate(${this.beta}deg);"><div class="leveltext">${this.beta}deg</div></div>
-      <div class="level" style="transform: rotate(${this.gamma}deg);"><div class="leveltext">${this.gamma}deg</div></div>
-      <div id="log" ></div>`;
+      <div class="level" style="transform: rotate(${this.gamma}deg);"><div class="leveltext">${this.gamma}deg</div></div>`
   }
 
 }
