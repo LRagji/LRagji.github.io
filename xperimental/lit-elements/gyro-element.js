eeedf3aaf9384635aef9dde7cd52c2b0926e9ca2
LRagji <Laukik.Ragji@live.com>
2018-10-03 20:27:33 +0530

    Added Picth and roll calculations

--- xperimental/lit-elements/gyro-element.js
+++ xperimental/lit-elements/gyro-element.js
@@ -38,18 +38,26 @@ class gyroElement extends LitElement {
     //Setting context for all methods
     this.handleMotion = this.handleMotion.bind(this);
     this.dispose = this.dispose.bind(this);
+    this._calculateRoll=this._calculateRoll.bind(this);
+    this._calculatePitch=this._calculatePitch.bind(this);
+    this._convertRadianToDegrees=this._convertRadianToDegrees.bind(this);
 
     if (window.DeviceMotionEvent) {
       window.addEventListener('devicemotion', this.handleMotion);
-      // setInterval(() => {
-      //   this.handleMotion({
-      //     rotationRate: {
-      //       alpha: (Math.random() * 100),
-      //       beta: (Math.random() * 100),
-      //       gamma: (Math.random() * 100)
-      //     }
-      //   })
-      // }, 1000);
+      setInterval(() => {
+        this.handleMotion({
+          rotationRate: {
+            alpha: (Math.random() * 100),
+            beta: (Math.random() * 100),
+            gamma: (Math.random() * 100)
+          },
+          accelerationIncludingGravity:{
+            x: (Math.random() * 100),
+            y: (Math.random() * 100),
+            z: (Math.random() * 100)
+          }
+        })
+      }, 1000);
 
     }
   }
@@ -65,34 +73,55 @@ class gyroElement extends LitElement {
 
   handleMotion(e) {
 
-    let dispatchEvent = false;
-    if (Math.abs(this.alpha - e.rotationRate.alpha) > this.alphaSensitivity) {
-      this.alpha = Math.round(e.rotationRate.alpha);
-      if (this.isSupported !== true) this.isSupported = true;
-      dispatchEvent = true;
-    }
+    // let dispatchEvent = false;
+    // if (Math.abs(this.alpha - e.rotationRate.alpha) > this.alphaSensitivity) {
+    //   this.alpha = Math.round(e.rotationRate.alpha);
+    //   if (this.isSupported !== true) this.isSupported = true;
+    //   dispatchEvent = true;
+    // }
 
-    if (Math.abs(this.beta - e.rotationRate.beta) > this.betaSensitivity) {
-      this.beta = Math.round(e.rotationRate.beta);
-      if (this.isSupported !== true) this.isSupported = true;
-      dispatchEvent = true;
-    }
+    // if (Math.abs(this.beta - e.rotationRate.beta) > this.betaSensitivity) {
+    //   this.beta = Math.round(e.rotationRate.beta);
+    //   if (this.isSupported !== true) this.isSupported = true;
+    //   dispatchEvent = true;
+    // }
 
-    if (Math.abs(this.gamma - e.rotationRate.gamma) > this.gammaSensitivity) {
-      this.gamma = Math.round(e.rotationRate.gamma);
-      if (this.isSupported !== true) this.isSupported = true;
-      dispatchEvent = true;
-    }
+    // if (Math.abs(this.gamma - e.rotationRate.gamma) > this.gammaSensitivity) {
+    //   this.gamma = Math.round(e.rotationRate.gamma);
+    //   if (this.isSupported !== true) this.isSupported = true;
+    //   dispatchEvent = true;
+    // }
 
-    if (dispatchEvent === true) {
-      this.dispatchEvent(new CustomEvent("data-changed", {
-        detail: {
-          "alpha": this.alpha,
-          "gamma": this.gamma,
-          "beta": this.beta
-        }
-      }))
+    // if (dispatchEvent === true) {
+    //   this.dispatchEvent(new CustomEvent("data-changed", {
+    //     detail: {
+    //       "alpha": this.alpha,
+    //       "gamma": this.gamma,
+    //       "beta": this.beta
+    //     }
+    //   }))
+    // }
+
+    if (e.accelerationIncludingGravity.x !== null &&
+      e.accelerationIncludingGravity.y !== null &&
+      e.accelerationIncludingGravity.z !== null) {
+      this.alpha = this._calculateRoll(e.accelerationIncludingGravity.y, e.accelerationIncludingGravity.z, this._convertRadianToDegrees);
+      this.beta = this._calculatePitch(e.accelerationIncludingGravity.x,e.accelerationIncludingGravity.y, e.accelerationIncludingGravity.z, this._convertRadianToDegrees);
+      if (this.isSupported !== true) this.isSupported = true;
     }
+  }
+
+  _convertRadianToDegrees(radians) {
+    return radians * 180.0 / Math.PI;
+  }
+
+  _calculateRoll(y, z, conversionFunction) {
+    return conversionFunction(Math.atan2(y, z));
+  }
+
+  _calculatePitch(x, y, z, conversionFunction) {
+    //atan2((- x_Buff) , sqrt(y_Buff * y_Buff + z_Buff * z_Buff)) * 57.3;
+    return conversionFunction(Math.atan2((x * -1), Math.sqrt((y * y + z))));
   }
 
   render() {
@@ -115,9 +144,9 @@ class gyroElement extends LitElement {
   } 
   </style>
   <div style="display:${this.isSupported?'visible':'none'}" >
-      <div class="level" style="transform: rotate(${this.alpha}deg);"><div class="leveltext" style="transform: rotate(${this.alpha*-1}deg);">${this.alpha}</div></div>
-      <div class="level" style="transform: rotate(${this.beta}deg);"><div  class="leveltext" style="transform: rotate(${this.beta *-1}deg);">${this.beta}</div></div>
-      <div class="level" style="transform: rotate(${this.gamma}deg);"><div  class="leveltext" style="transform: rotate(${this.gamma *-1}deg);">${this.gamma}</div></div>
+      <div class="level" style="transform: rotate(${this.alpha}deg);"><div class="leveltext" style="transform: rotate(${this.alpha*-1}deg);">${parseFloat(this.alpha).toFixed(2)}</div></div>
+      <div class="level" style="transform: rotate(${this.beta}deg);"><div  class="leveltext" style="transform: rotate(${this.beta *-1}deg);">${parseFloat(this.beta).toFixed(2)}</div></div>
+      <div class="level" style="transform: rotate(${this.gamma}deg);"><div  class="leveltext" style="transform: rotate(${this.gamma *-1}deg);">${parseFloat(this.gamma).toFixed(2)}</div></div>
   </div>
   <div style="display:${!this.isSupported?'visible':'none'}" >
   <center><p><b>Your browser doesnot support gyro api :(</b><br/>Works only on chrome.</p></center>
